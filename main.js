2484ad57b89f018ec19e18f148009c4604b9d451
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2020-08-23 22:16:07 +0500

    blank classes Cart and CartItem done

--- main.js
+++ main.js
@@ -11,33 +11,31 @@ const products = [
 ];
 
 class ProductItem {
-    constructor(title = 'product', price = 0){
+    constructor(title = 'product', price = 0) {
         this.title = title;
         this.price = price;
     }
 
-    render(){
+    render() {
         return `
             <div class="fetured_item">
-                <a href="Single_page.html"><img src="img/man_product-1.jpg" alt="product image" class="fetured_img"></a>
+                <a href="Single_page.html"><img src="https://via.placeholder.com/263x280" alt="product image" class="fetured_img"></a>
                 <div class="fetured_text"> <a href="#" class="fetured_description">${this.title}</a>
                     <p class="fetured_price"><span class="pink_text">$${this.price}</span></p>
                 </div><a href="#" class="product_add_2">Add to Cart</a>
-                <a href="#" class="product_refresh"></a>
-                <a href="#" class="product_like"></a>
             </div>
         `;
     }
 }
 
 class ProductList {
-    constructor(){
+    constructor() {
         this.list = [];
     }
-    _fetchProductList(){
+    _fetchProductList() {
         this.list = products;
     }
-    render(){
+    render() {
         let html = '';
         this._fetchProductList();
         this.list.forEach(({ title, price }) => {
@@ -45,6 +43,53 @@ class ProductList {
         });
         document.querySelector('.product_container').innerHTML = html;
     }
+}
+
+class CartItem {
+    constructor(product) {
+        this.title = product.title;
+        this.price = product.price;
+        this.quantity = 0;
+    }
+    render() {
+        return
+        `
+            <a href="product.html">
+                <div class="drop-cart__good">
+                    <img class="drop-cart__img" src="https://via.placeholder.com/72x85" alt="photo">
+                    <div class="drop-cart__text">
+                        <h2 class="drop-cart__h2">${this.title}</h2>
+                        <img src="img/rating_stars.png" alt="rating">
+                        <p class="drop-cart__price pink_text">${this.quantity}  x   $${price}</p>
+                    </div>
+                        <img class="drop-cart__delete" src="img/delete.png" alt="delete">
+                </div>
+            </a>
+        `;
+    }
+}
+
+class Cart {
+    constructor() {
+        this.cartList = [];
+        this.visible = false;
+    }
+    show() {
+
+    }
+    add(product) {
+
+    }
+    delete(product) {
+
+    }
+    countTotal() {
+
+    }
+    render() {
+
+    }
+
 }
 
 let productList = new ProductList();
